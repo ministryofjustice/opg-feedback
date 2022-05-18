4071ea2d709fe735b2d6c25681779b5b3868ad02
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-05-18 16:55:47 +0100

    now saves line to feedback table

--- rest-api/app/extra__init__.py
+++
@@ -1,6 +0,0 @@
-class Feedback(app.db.Model):
-    __tablename__ = 'perf_feedback',
-    id = app.db.Column('id', app.db.Integer, primary_key=True),
-    rating = app.db.Column('rating', app.db.Integer, nullable=False),
-    comment = app.db.Column('comment', app.db.String(1200), nullable=False),
-    datetime = app.db.Column('datetime', app.db.DateTime(), nullable=False)
--- rest-api/app/extra_routes.py
+++ rest-api/app/extra_routes.py
@@ -3,6 +3,18 @@ import os
 from datetime import datetime
 from flask import request
 
+class Feedback(app.db.Model):
+    __tablename__ = 'perf_feedback'
+    id = app.db.Column('id', app.db.Integer, primary_key=True, autoincrement=True)
+    rating = app.db.Column('rating', app.db.Integer, nullable=False)
+    comment = app.db.Column('comment', app.db.String(1200), nullable=False)
+    datetime = app.db.Column('datetime', app.db.DateTime(), nullable=False)
+
+    def __init__(self, rating, comment, datetime):
+        self.rating = rating
+        self.comment = comment
+        self.datetime = datetime
+
 incomes = [
   { 'description': 'salary', 'amount': 5000 }
 ]
@@ -16,9 +28,10 @@ def add_income():
   incomes.append(request.get_json())
   return '', 204
 
-@app.route('/feedback', methods=['POST'])
+@app.route('/feedback')
 def post_feedback():
-    # create instance of Feedback object, save to db.  1st hardcode it, then take params
-    feedback = Feedback(rating = 1, comment = "service kicks ass", datetime = datetime.now())
-    app.db.session.add(feedback)
-    app.db.session.commit()
+  # create instance of Feedback object, save to db.  1st hardcode it, then take params
+  feedback = Feedback(rating = 1, comment = "Very happy with the service", datetime = datetime.now())
+  app.db.session.add(feedback)
+  app.db.session.commit()
+  return '', 204
