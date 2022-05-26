12d0bb596ff3734c1d0b76a432f49ff980cdf43b
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-05-26 16:57:31 +0100

    tests look for data in table

--- rest-api/tests/test_feedback.py
+++ rest-api/tests/test_feedback.py
@@ -1,6 +1,9 @@
 import json
 import requests
 import pytest
+import sys
+sys.path.append('../app:..')
+from feedback import Feedback
 
 url = "http://localhost:8005"
 
@@ -20,6 +23,10 @@ def test_save_feedback():
 
     test_headers = {"Content-Type": "application/json"}
 
+    # ensure we don't already have saved data before we start the test
+    feedback = Feedback.query.filter_by(comment='Very happy with the service').all()
+    assert len(feedback) == 0
+
     #r = requests.post(
     #    server.url + "/healthcheck", headers=test_headers, data=json.dumps(test_data)
     #)
@@ -28,4 +35,10 @@ def test_save_feedback():
         url + "/feedback"
     )
     assert r.status_code == 201
-#    assert r.json() == expected_return
+
+    # ensure we have exactly 1 comment saved
+    feedback = Feedback.query.filter_by(comment='Very happy with the service').all()
+    assert len(feedback) == 1
+
+    db.session.delete(feedback)
+    db.session.commit()
