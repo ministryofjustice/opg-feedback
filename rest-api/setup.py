eca0feb7f29b7d0f093bd3a0cb16608c561fee0f
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-05-27 11:11:07 +0100

    use setup.py

---
+++ rest-api/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(name='feedbackapi', 
+        version='1.0', 
+        description='Feedback plugins for flask api',
+        packages=find_packages())
