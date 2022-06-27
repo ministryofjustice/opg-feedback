f20916f140287e7f40f6309a2015d8cab33ee622
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-06-27 11:38:28 +0100

    missing files

---
+++ rest-api/opgfeedbackapi/__init__.py
@@ -0,0 +1,16 @@
+import os
+from opgflaskapi import create_flask_app
+#from .feedback_blueprint import feedback_blueprint
+from .incomes_blueprint import incomes_blueprint
+
+postgres_url = 'postgresql://{}:{}@{}/{}'.format(
+        os.getenv('POSTGRES_USERNAME') ,
+        os.getenv('POSTGRES_PASSWORD') ,
+        os.getenv('POSTGRES_HOSTNAME') ,
+        os.getenv('POSTGRES_NAME'))
+
+api = create_flask_app("feedback", postgres_url)
+
+# TODO for now just test simple incomes blueprint which has no deps,  once that works, do feedback blueprint too
+api.register_blueprint(incomes_blueprint)
+#api.register_blueprint(feedback_blueprint)
