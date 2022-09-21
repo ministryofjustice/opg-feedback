3637a99f9f3239341620b0d4b4a8b4aec5c3cb74
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-09-21 11:52:53 +0100

    dev section for setup.py

--- feedbackfront/setup.py
+++ feedbackfront/setup.py
@@ -9,6 +9,12 @@ setup(
         "govuk-frontend-wtf>=0.3.0",
         "gunicorn>=20.1.0,<21",
     ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "pytest-cov",
+        ],
+    },
     packages=["opgfeedbackfront"],
     include_package_data=True,
 )
