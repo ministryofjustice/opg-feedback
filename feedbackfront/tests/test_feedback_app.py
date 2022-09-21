5f4bce49dcc51668433f1ef5d25720e073131609
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-09-21 11:33:43 +0100

    add tests for branding

--- feedbackfront/tests/test_feedback_app.py
+++ feedbackfront/tests/test_feedback_app.py
@@ -11,3 +11,10 @@ def test_create_feedback_app():
 
     response = app.test_client().get("/completed-feedback")
     assert response.status_code == 200
+
+    # although access to assets is tested in opg-flask-front, retest to make sure works in this context also
+    response = app.test_client().get("/assets/govuk-frontend-3.14.0.min.css")
+    assert response.status_code == 200
+
+    response = app.test_client().get("/assets/govuk-frontend-3.14.0.min.js")
+    assert response.status_code == 200
