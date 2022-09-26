eb2887f0c2061c4792ade7609bb1e17c59cf8d81
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-09-26 17:24:15 +0100

    bump version

--- feedbackfront/setup.py
+++ feedbackfront/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="opgfeedbackfront",
-    version="1.0",
+    version="1.1",
     description="Feedback plugins for flask app",
     install_requires=[
         "opgflaskfront @ git+https://github.com/ministryofjustice/opg-flask-apps.git#subdirectory=flaskfront",
