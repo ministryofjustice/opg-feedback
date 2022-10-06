267dcf17fcd6acb832da5792ac9e79e4b7feaa82
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-10-06 15:10:42 +0100

    add laoder for templates

--- feedbackfront/opgfeedbackfront/__init__.py
+++ feedbackfront/opgfeedbackfront/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from opgflaskfront import create_flask_app
 from jinja2 import PackageLoader
 from .feedbackfront_blueprint import feedbackfront_blueprint
+from jinja2 import PrefixLoader
+from flask_wtf.csrf import CSRFProtect
+from govuk_frontend_wtf.main import WTFormsHelpers
 
 
 def create_feedback_app(name: str, force_https=False) -> Flask:
@@ -9,7 +12,10 @@ def create_feedback_app(name: str, force_https=False) -> Flask:
     app = create_flask_app(
         "feedbackfront",
         force_https=force_https,
-        loaders=[PackageLoader("opgfeedbackfront")],
+        loaders=[
+            PackageLoader("opgfeedbackfront"),
+            PrefixLoader({"govuk_frontend_wtf": PackageLoader("govuk_frontend_wtf")}),
+        ],
     )
 
     app.register_blueprint(feedbackfront_blueprint)
